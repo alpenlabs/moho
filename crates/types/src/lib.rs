393b70adbb38966df9c0056e819a7b1e4f6b8758
Prajwol Gyawali <prajwol@alpenlabs.io>
2025-07-08 20:18:25 +0545

    refactor: remove proof in favor of zkaleido

--- crates/types/src/lib.rs
+++ crates/types/src/lib.rs
@@ -3,13 +3,11 @@
 mod constants;
 mod id;
 mod merkle;
-mod proof;
 mod relation;
 mod state;
 
 pub use constants::*;
 pub use id::{InnerStateCommitment, MohoStateCommitment, StateReference};
 pub use merkle::{MerkleProof, MerkleTree};
-pub use proof::{MockProofSystem, ProofSystem};
 pub use relation::{MohoAttestation, StateRefAttestation};
 pub use state::{ExportContainer, ExportEntry, ExportState, MohoState};
